c2438d7664b1c4f1b2c64a7014105d7583095c4f
Sabrina Graham <techiepotato3004@gmail.com>
2021-11-09 20:41:49 -0500

    Added Lookup Cities button, context query parameter and new html table for city results

--- world.js
+++ world.js
@@ -1,25 +1,37 @@
 document.addEventListener('DOMContentLoaded',(event)=>{
-    let lookupbtn=document.getElementById('lookup');
+    let countrybtn=document.getElementById('lookup');
+    let citiesbtn=document.getElementById('cities');
     let country=document.getElementById('country');
     let result=document.getElementById('result');
+    let input;
+    let q;
     
-
-    //when search button is clicked
-    lookupbtn.addEventListener('click',e=>{
-        e.preventDefault();
-
-        result.innerHTML=''; //clear result container after each search
-        
-        let input=country.value; //get input value from search bar
-
-        //AJAX GET Request
+    ////AJAX GET Request
+    function ajaxRequest(query)
+    {
         httpRequest=new XMLHttpRequest();
         var url='http://localhost/info2180-lab5/world.php'
         httpRequest.onreadystatechange=loadResults;
-        url+="?country="+input;
+        url+=query;
         httpRequest.open('GET',url);
         httpRequest.send();
+    }
+
+    //when lookup buttons are clicked
+    countrybtn.addEventListener('click',e=>{
+        e.preventDefault();
+        input=country.value; //get input value from search bar
+        result.innerHTML=''; //clear result container after each search
+        q="?country="+input+"&context=";
+        ajaxRequest(q);
     });
+    citiesbtn.addEventListener('click',e=>{
+        e.preventDefault();
+        input=country.value; //get input value from search bar
+        result.innerHTML='';
+        q="?country="+input+"&context=cities";
+        ajaxRequest(q);
+    })
 
     //Function to retrieve info from php file
     function loadResults()
--- world.php
+++ world.php
@@ -5,39 +5,86 @@ $username = 'lab5_user';
 $password = 'password123';
 $dbname = 'world';
 
-$q=htmlspecialchars($_GET["country"]);
-//echo $q;
-
 $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
-$stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$q%'");
 
-$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
-//var_dump($stmt);
+$q=htmlspecialchars($_GET["country"]);
 
-//echo json_encode($results);
+
+if (htmlspecialchars($_GET["context"])!=null){
+    
+    
+    $stmt = $conn->query("SELECT* FROM countries JOIN cities ON cities.country_code=countries.code WHERE countries.name LIKE '%$q%'");
+
+    $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+	echo "<table>";
+	echo "<thead>";
+	echo "<tr>";
+	echo "<th>Name</th>";
+	echo "<th>District</th>";
+	echo "<th>Population</th>";
+	echo "</tr>";
+	echo "</thead>";
+    echo "<tbody>";
+	foreach ($results as $result)
+	{
+		echo "<tr>";
+		echo "<td>";
+		echo $result['name'];
+		echo "</td>";
+		echo "<td>"; 
+		echo $result['district'];
+		echo "</td>";
+		echo "<td>";
+		echo $result['population'];
+		echo "</td>";
+		echo "</tr>";
+	}
+	echo "</tbody>";
+	echo "</table>";
+}
+else {
+	
+	
+    $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$q%'");
+
+    $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+	echo "<table>";
+	echo "<thead>";
+	echo "<tr>";
+	echo "<th>Country Name</th>";
+	echo "<th>Continent</th>";
+	echo "<th>Independence Year</th>";
+	echo "<th>Head of State</th>";
+	echo "</tr>";
+	echo "</thead>";
+    echo "<tbody>";
+	foreach ($results as $result)
+	{
+		echo "<tr>";
+		echo "<td>";
+		echo $result['name']; 
+		echo "</td>";
+		echo "<td>"; 
+		echo $result['continent'];
+		echo "</td>";
+		echo "<td>";
+		echo $result['independence_year'];
+		echo "</td>";
+		echo "<td>";
+		echo $result['head_of_state'];
+		echo "</td>";
+		echo "</tr>";
+	}
+	echo "</tbody>";
+	echo "</table>";
+
+}
+    
+
+
+
 
 
 ?>
-<link rel="stylesheet" href="styles.css" type="text/css" />
-<table>
-	<thead>
-	    <tr>
-		    <th>Country Name</th>
-			<th>Continent</th>
-			<th>Independence Year</th>
-			<th>Head of State</th>
-		</tr>
-	</thead>
-    <tbody>
-		<?php foreach ($results as $result): ?>
-			<tr>
-				<td><?= $result['name']; ?></td>
-				<td><?= $result['continent']; ?></td>
-				<td><?= $result['independence_year']; ?></td>
-				<td><?= $result['head_of_state']; ?></td>
-			</tr>
-		<?php endforeach; ?>
-	</tbody>
-</table>
-
-
